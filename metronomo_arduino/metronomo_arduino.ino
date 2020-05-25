1090675bdb0ad1c0f29169e70acea184d41825c7
BRUNO RIBEIRO CHAVES <53835637+brunorchaves@users.noreply.github.com>
2020-05-24 21:23:00 -0300

    Display code

--- metronomo_arduino/metronomo_arduino.ino
+++ metronomo_arduino/metronomo_arduino.ino
@@ -8,7 +8,7 @@
  *  Authors:
  *  Bruno Ribeiro (https://github.com/brunorchaves)
  */
-
+#include "SSD1306.h"
 #define inputCLK 13
 #define inputDT 12
 //Pins***************
@@ -32,10 +32,24 @@ int previousStateCLK;
 int currentStateCLK;
 int counter = 1; 
 //Timer
-int timerDivider = 1000;
+int timerDivider = 100;
 int g_milisecs = 0;
 hw_timer_t *timer = NULL;
 //portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
+
+//Display------------------------------------------------
+//objeto controlador do display de led
+/*
+  0x3c  :   é um identificador único para comunicação do display
+  pino 5 e 4 são os de comunicação (SDA, SDC)   
+*/
+#define SDA_PIN 21                      // GPIO21 -> SDA
+#define SCL_PIN 22                      // GPIO22 -> SCL
+#define SSD_ADDRESS 0x3c                // Endereço do display no bus i2c
+SSD1306  screen(SSD_ADDRESS, SDA_PIN, SCL_PIN);
+ 
+//utilizado para fazer o contador de porcentagem
+int contador;
 
 void IRAM_ATTR onTimer()
 {
@@ -47,7 +61,7 @@ void setup()
 {
   //Serial Monitor-----------------------------
   Serial.begin (9600);
-  //Timer
+  //Timer--------------------------------------
   timer = timerBegin(0,80,true);//Timer 0, MWDT clock period = 12,5 s * TIMGn_Tx_WDT_CLK_PRESCALE -> 12.5ns*80 ->1u sec, countUP 
   timerAttachInterrupt(timer,&onTimer, true); //edge (not level) triggered
   timerAlarmWrite(timer, 1000, true);//1000*1us = 1s , autoreload true;
@@ -59,6 +73,13 @@ void setup()
   pinMode (inputCLK,INPUT);
   pinMode (inputDT,INPUT);
   previousStateCLK = digitalRead(inputCLK);
+  //Display-------------------------------------
+  // Inicializa o objeto que controlará o que será exibido na tela
+  screen.init();
+  //gira o display 180º (deixa de ponta cabeça)
+ // display.flipScreenVertically();
+  //configura a fonte de escrita "ArialMT_Plain_10"
+  screen.setFont(ArialMT_Plain_10);
 
 }
 
@@ -154,7 +175,47 @@ int encoder()
 
   return counter;
 }
-
+void drawProgressBar(int BPM) 
+{
+  //Serial.print(">> ");
+  //Serial.println(contador);
+   
+  // desenha a progress bar
+  /*
+   * drawProgressBar(x, y, width, height, value);
+    parametros (p):
+      p1: x       --> coordenada X no plano cartesiano
+      p2: y       --> coordenada Y no plano cartesiano
+      p3: width   --> comprimento da barra de progresso
+      p4: height  --> altura da barra de progresso
+      p5: value   --> valor que a barra de progresso deve assumir
+     
+  */
+  screen.drawProgressBar(10, 32, 100, 10, BPM);
+ 
+  // configura o alinhamento do texto que será escrito
+  //nesse caso alinharemos o texto ao centro
+  screen.setTextAlignment(TEXT_ALIGN_CENTER);
+   
+  //escreve o texto de porcentagem
+  /*
+   * drawString(x,y,text);
+    parametros (p):
+     p1: x      --> coordenada X no plano cartesiano
+     p2: y      --> coordenada Y no plano cartesiano
+     p3: string --> texto que será exibido
+  */
+  screen.drawString(64, 15, String(BPM));
+ 
+  //se o contador está em zero, escreve a string "valor mínimo"
+  // if(contador == 0){
+  //   screen.drawString(64, 45, "Valor mínimo");
+  // }
+  //se o contador está em 100, escreve a string "valor máximo"
+  // else if(contador == 100){
+  //   screen.drawString(64, 45, "Valor máximo");
+  // }
+}
 enum
 {
 	State_getBPM = 0,
@@ -196,7 +257,20 @@ void loop() {
     default:
       break;
   }
- 
+  if(--timerDivider == 0)
+  {//limpa todo o display, apaga o contúdo da tela
+    timerDivider = 100;
+    screen.clear();
+    
+    //  ++counter;
+    //  counter > 100 ? counter = 0 : counter = counter;  
+  
+    //desenha a progress bar
+    drawProgressBar(valor_encoder);
+  
+    //exibe na tela o que foi configurado até então.
+    screen.display();
+  }
   machineMotorcontrol(BPM);
 
   Serial.print(currentEncoder);
