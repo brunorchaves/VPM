3fe1de23c953071356b4b2e47ff02f8e448bf450
BRUNO RIBEIRO CHAVES <53835637+brunorchaves@users.noreply.github.com>
2020-05-25 01:26:16 -0300

    Encoder Library and display update

--- VPM/VPM.ino
+++ VPM/VPM.ino
@@ -9,8 +9,7 @@
  *  Bruno Ribeiro (https://github.com/brunorchaves)
  */
 #include "SSD1306.h"
-#define inputCLK 13
-#define inputDT 12
+#include <AiEsp32RotaryEncoder.h> //Source: https://github.com/igorantolic/ai-esp32-rotary-encoder
 //Pins***************
 //Device          ESP
 //Encoder-->      
@@ -20,6 +19,64 @@
 //Display--> 
 //SCL             D22
 //SDA             D21
+
+/*
+connecting Rotary encoder
+CLK (A pin) - to any microcontroler intput pin with interrupt -> in this example pin 32
+DT (B pin) - to any microcontroler intput pin with interrupt -> in this example pin 21
+SW (button pin) - to any microcontroler intput pin -> in this example pin 25
+VCC - to microcontroler VCC (then set ROTARY_ENCODER_VCC_PIN -1) or in this example pin 25
+GND - to microcontroler GND
+*/
+#define ROTARY_ENCODER_A_PIN 32
+#define ROTARY_ENCODER_B_PIN 34
+#define ROTARY_ENCODER_BUTTON_PIN 25
+#define ROTARY_ENCODER_VCC_PIN 27 /*put -1 of Rotary encoder Vcc is connected directly to 3,3V; else you can use declared output pin for powering rotary encoder */
+AiEsp32RotaryEncoder rotaryEncoder = AiEsp32RotaryEncoder(ROTARY_ENCODER_A_PIN, ROTARY_ENCODER_B_PIN, ROTARY_ENCODER_BUTTON_PIN, ROTARY_ENCODER_VCC_PIN);
+int16_t g_encoderValue = 0;
+int16_t g_encoderRead = 0;
+static int BPM = 120;
+void rotary_onButtonClick() 
+{
+  //rotaryEncoder.reset();
+  //rotaryEncoder.disable();
+  g_encoderValue = rotaryEncoder.readEncoder();
+  BPM = g_encoderValue;
+}
+void rotary_loop() 
+{
+  //first lets handle rotary encoder button click
+  if (rotaryEncoder.currentButtonState() == BUT_RELEASED) {
+    //we can process it here or call separate function like:
+    rotary_onButtonClick();
+  }
+
+  //lets see if anything changed
+  int16_t encoderDelta = rotaryEncoder.encoderChanged();
+  g_encoderRead = rotaryEncoder.readEncoder();
+  //optionally we can ignore whenever there is no change
+  if (encoderDelta == 0) return;
+  
+  //for some cases we only want to know if value is increased or decreased (typically for menu items)
+  //if (encoderDelta>0) Serial.print("+"); //Uncomment 4 debug
+  //if (encoderDelta<0) Serial.print("-"); //Uncomment 4 debug
+
+  //for other cases we want to know what is current value. Additionally often we only want if something changed
+  //example: when using rotary encoder to set termostat temperature, or sound volume etc
+  
+  //if value is changed compared to our last read
+  if (encoderDelta!=0) {
+    //now we need current value
+    int16_t encoderValue = rotaryEncoder.readEncoder();
+    //process new value. Here is simple output.
+    Serial.print("Value: ");
+    Serial.print(encoderValue);
+    Serial.println(" - Click to select");
+    
+  }
+  
+}
+
 
 //**Tip**--> every global variable must be preceeded by a "g"
 //Motor Variables
@@ -70,9 +127,12 @@ void setup()
   pinMode(g_motor, OUTPUT);
   //pinMode(g_led, OUTPUT);
   /// Encoder-----------------------------------
-  pinMode (inputCLK,INPUT);
-  pinMode (inputDT,INPUT);
-  previousStateCLK = digitalRead(inputCLK);
+    //we must initialize rorary encoder 
+  rotaryEncoder.begin();
+  rotaryEncoder.setup([]{rotaryEncoder.readEncoder_ISR();});
+  //optionally we can set boundaries and if values should cycle or not
+  rotaryEncoder.setBoundaries(40, 240, false); //minValue, maxValue, cycle values (when max go to min and vice versa)
+
   //Display-------------------------------------
   // Inicializa o objeto que controlará o que será exibido na tela
   screen.init();
@@ -129,52 +189,7 @@ void machineMotorcontrol(int BPM)
       break;
   }
 } 
-int encoder()
-{
-  int valor_encoder = 0;
-  // Read the current state of inputCLK
-   currentStateCLK = digitalRead(inputCLK);
-    
-   // If the previous and the current state of the inputCLK are different then a pulse has occured
-   if (currentStateCLK != previousStateCLK)
-   { 
-      // If the inputDT state is different than the inputCLK state then 
-     // the encoder is rotating counterclockwise
-     if (digitalRead(inputDT) != currentStateCLK) 
-     { 
-       counter --;
-       //encdir ="CCW";
-       //digitalWrite(ledCW, LOW);
-       //digitalWrite(ledCCW, HIGH);
-       
-     } 
-     else 
-     {
-       // Encoder is rotating clockwise
-       counter ++;
-      // encdir ="CW";
-       //digitalWrite(ledCW, HIGH);
-       //digitalWrite(ledCCW, LOW);
-     }
-    if(counter >=240)
-    {
-      counter = 240;
-    }
-    else if(counter <=1)
-    {
-      counter = 1;
-    }
-    //  Serial.print("Direction: ");
-    //  Serial.print(encdir);
-    //  Serial.print(" -- Value: ");
-    //  Serial.println(counter);
-   } 
-   
-   // Update previousStateCLK with the current state
-  previousStateCLK = currentStateCLK; 
 
-  return counter;
-}
 void drawProgressBar(int BPM) 
 {
   //Serial.print(">> ");
@@ -191,7 +206,7 @@ void drawProgressBar(int BPM)
       p5: value   --> valor que a barra de progresso deve assumir
      
   */
-  screen.drawProgressBar(10, 32, 100, 10, BPM);
+  //screen.drawProgressBar(10, 32, 100, 10, BPM);
  
   // configura o alinhamento do texto que será escrito
   //nesse caso alinharemos o texto ao centro
@@ -205,7 +220,8 @@ void drawProgressBar(int BPM)
      p2: y      --> coordenada Y no plano cartesiano
      p3: string --> texto que será exibido
   */
-  screen.drawString(64, 15, String(BPM));
+  screen.drawString(64, 15, "BPM");
+  screen.drawString(64, 30, String(BPM));
  
   //se o contador está em zero, escreve a string "valor mínimo"
   // if(contador == 0){
@@ -222,41 +238,42 @@ enum
 	State_ChangeBPM,
 };
 void loop() {
-  static int BPM = 60;
+  
   static int state = 0;
   static int currentEncoder = 1;
   static int previousEncoder = 1;
   static int counter = 0;
   static int valor_encoder = 1;
   static bool move = false;
-  
-  currentEncoder = encoder();
+   //in loop call your custom function which will process rotary encoder values
+  rotary_loop();
+  //currentEncoder = encoder();
   
   counter++;
-  switch(state)
-  {
-    case State_getBPM:
-      if(currentEncoder != previousEncoder)
-      {
-        move = true;
-        previousEncoder = currentEncoder;
-        counter = 0;
-      }
-      if(counter >= 15 && move == true)
-      {
-        valor_encoder = currentEncoder;
-        move = false;
-        state = State_ChangeBPM;
-      }
-      break;
-    case State_ChangeBPM:
-      BPM = valor_encoder;
-      //previousEncoder = currentEncoder;
-      state = State_getBPM;
-      break;
-    default:
-      break;
-  }
+  // switch(state)
+  // {
+  //   case State_getBPM:
+  //     if(currentEncoder != previousEncoder)
+  //     {
+  //       move = true;
+  //       previousEncoder = currentEncoder;
+  //       counter = 0;
+  //     }
+  //     if(counter >= 15 && move == true)
+  //     {
+  //       valor_encoder = currentEncoder;
+  //       move = false;
+  //       state = State_ChangeBPM;
+  //     }
+  //     break;
+  //   case State_ChangeBPM:
+  //     BPM = valor_encoder;
+  //     //previousEncoder = currentEncoder;
+  //     state = State_getBPM;
+  //     break;
+  //   default:
+  //     break;
+  // }
   if(--timerDivider == 0)
   {//limpa todo o display, apaga o contúdo da tela
     timerDivider = 100;
@@ -266,18 +283,18 @@ void loop() {
     //  counter > 100 ? counter = 0 : counter = counter;  
   
     //desenha a progress bar
-    drawProgressBar(valor_encoder);
+    drawProgressBar(g_encoderRead);
   
     //exibe na tela o que foi configurado até então.
     screen.display();
   }
   machineMotorcontrol(BPM);
 
-  Serial.print(currentEncoder);
+  Serial.print(g_encoderRead);
   Serial.print(" ");
   Serial.print(BPM);
   Serial.print(" ");
   Serial.println(counter);
+  if (millis()>20000) 
+    rotaryEncoder.enable ();
 }
-
-
